bfb8abfcd1d8cf77db7fe8a82ceb0c0b9e86d1ed
reisleo99 <leonardo.ao.reis@gmail.com>
2024-09-15 18:32:12 -0400

    Cleaning up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-const token = import.meta.env.VITE_GITHUB_TOKEN;
 
-import Nav from "./components/Nav"; // if you have a Nav component
+import Nav from "./components/Nav";
 
 const App: React.FC = () => {
   return (
